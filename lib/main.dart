34a86092bd85f2f703734826bf37e38a76c4bf3c
vrishanknehru <vrishank06@gmail.com>
2025-07-01 11:13:53 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hello, World!')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hello App')),
+        body: Center(child: Text('Hello, World!')),
+      ),
+    ),
+  );
 }
